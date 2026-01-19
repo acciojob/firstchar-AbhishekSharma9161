baf671b8e2aa25acad77af6c88c0a22756b960c8
Abhishek Sharma <93138322+AbhishekSharma9161@users.noreply.github.com>
2026-01-19 10:20:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 function firstChar(str) {
-  // your code here
-for(let i =0 l i <str.length ; i++){
-if(str[i]!== ''){
-return str[i];
-
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      return str[i];
+    }
+  }
+  return '';
 }
-
-}
-
-
-}
-
-// Do not change the code below
-//Uncomment the following line to show the prompt popup
-const text = prompt("Enter text:");
-alert(firstChar(text));
